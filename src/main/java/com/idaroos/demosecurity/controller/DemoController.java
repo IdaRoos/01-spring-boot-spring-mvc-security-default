0bf47156ee23ab9d3c390b4b13ef7f0c1854cade
IdaRoos <ida_r@live.se>
2023-06-08 16:43:24 +0200

    updated methods

--- src/main/java/com/idaroos/demosecurity/controller/DemoController.java
+++ src/main/java/com/idaroos/demosecurity/controller/DemoController.java
@@ -1,2 +1,32 @@
-package com.idaroos.demosecurity.controller;public class DemoController {
+package com.idaroos.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+
+
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    // add a request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
+    // add request mapping for denied
+    @GetMapping("/access-denied")
+        public String showDenied(){
+            return "denied";
+        }
 }
--- src/main/java/com/idaroos/demosecurity/controller/LoginController.java
+++ src/main/java/com/idaroos/demosecurity/controller/LoginController.java
@@ -1,2 +1,13 @@
-package com.idaroos.demosecurity.controller;public class LoginController {
+package com.idaroos.demosecurity.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "fancy-login";
+    }
 }
